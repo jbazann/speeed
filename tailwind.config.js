ce2b6082dfc922e6bb3e22929cd543bfdecebdb4
jbazann <joa7.baz@gmail.com>
2024-10-02 15:02:18 -0300

    add tailwind toolchain

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{js,html}",
+    "./src/**/*.{js,html}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
