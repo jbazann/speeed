0851a2739942cb0f4a4d085f14b15b2aa9fc0070
jbazann <joa7.baz@gmail.com>
2024-10-04 21:05:08 -0300

    custom shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        'strong': '0 2rem 1.5rem rgba(0, 0, 0, 1)',
+        'medium': '0 1rem 0.75rem rgba(0, 0, 0, 0.5)',
+      }
+    },
   },
   plugins: [],
 }
