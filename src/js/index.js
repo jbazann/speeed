f98dbe6be42f90044b0586c2aef3efffc95a9c0a
jbazann <joa7.baz@gmail.com>
2024-10-04 21:11:42 -0300

    getTemplateHTMLById

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import Alpine from 'alpinejs';
 import content1 from './content1'
+import templateUtils from './templateUtils'
 
 window.Alpine = Alpine;
 Alpine.data('content1',content1);
+Alpine.data('templateUtils',templateUtils);
 Alpine.start();
