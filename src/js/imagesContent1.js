2eaede6774e840c7867142627a2685b9a1537a72
jbazann <joa7.baz@gmail.com>
2024-10-07 15:05:30 -0300

    restore slider images

--- src/js/imagesContent1.js
+++ src/js/imagesContent1.js
@@ -6,5 +6,11 @@ export default () => ({
 
 const images = [
     '/img/wip.webp',
-    '/img/base.png'
+    '/img/wide1.jpg',
+    '/img/wide2.jpg',
+    '/img/wide3.jpg',
+    '/img/wide4.jpg',
+    '/img/wide5.jpg',
+    '/img/wide6.jpg',
+    '/img/wide7.jpg',
 ];
