3410cccf13dae7e0268e9270d331a22a07be7ba0
jbazann <joa7.baz@gmail.com>
2024-10-19 20:39:58 -0300

    Remove overlay.

--- src/js/content1.js
+++ src/js/content1.js
@@ -4,40 +4,20 @@ export default () => ({
     currentImage,
     nextImage,
     previousImage,
-    overlay,
-    overlayData
 })
 
 const images = imagesContent1().images;
 
-const overlays = [
-    {
-        image: '/img/wip-sign.png',
-        class: 'size-2/5 self-center mt-2 drop-shadow-strong'
-    }
-]
-
 let currentImage = images[0];
 let currentIndex = 0;
-let overlayData = overlay();
+
 function nextImage() {
     currentIndex = (currentIndex+1)%images.length;
     currentImage = images[currentIndex];
-    overlay();
     return currentImage;
 }
 function previousImage() {
     currentIndex = (currentIndex+images.length-1)%images.length;
     currentImage = images[currentIndex];
-    overlay();
     return currentImage;
-}
-function overlay() {
-    const data = {overlayShow: false, overlayImage: '', overlayClass: ''};
-    if(currentIndex === 0) {
-        data.overlayShow = true;
-        data.overlayImage = overlays[0].image;
-        data.overlayClass = overlays[0].class;
-    }
-    return data;
 }
