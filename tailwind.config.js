be53577e170d41ef7be42f223e327b4e2798996a
jbazann <joa7.baz@gmail.com>
2024-10-19 20:40:15 -0300

    Add custom colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'jblack': '#01060C'
+      },
       dropShadow: {
         'strong': '0 2rem 1.5rem rgba(0, 0, 0, 1)',
         'medium': '0 1rem 0.75rem rgba(0, 0, 0, 0.5)',
