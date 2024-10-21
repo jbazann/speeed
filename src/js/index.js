f0482533bd3ec09e5f56ee9cdf53be137bc93414
jbazann <joa7.baz@gmail.com>
2024-10-20 22:50:31 -0300

    Add social buttons

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import Alpine from 'alpinejs';
 import imagesContent1 from './imagesContent1'
 import content1 from './content1'
 import templateUtils from './templateUtils'
+import socialButtons1 from './socialButtons1'
 
 window.Alpine = Alpine;
 Alpine.data('content1',content1);
 Alpine.data('imagesContent1',imagesContent1);
 Alpine.data('templateUtils',templateUtils);
+Alpine.data('socialButtons1',socialButtons1);
 Alpine.start();
---
+++ src/js/socialButtons1.js
@@ -0,0 +1,10 @@
+
+export default () => ({
+    btns
+});
+
+const btns = [
+        {src: '/img/x-logo-white.png', alt: 'Twitter/X logo.', label: 'NOT YET'},
+        {src: '/img/github-mark-white.svg', alt: 'GitHub logo.', label: 'github.com/jbazann'},
+        {src: '/img/In-White-40@2x.png', alt: 'LinkedIn logo', label: 'linkedin.com/in/jbazann/'}
+    ];
