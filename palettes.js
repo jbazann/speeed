cbacf1cce67558083fb98d538503d9ffbb398fd1
jbazann <joa7.baz@gmail.com>
2024-12-21 20:10:13 -0300

    I totally did not forget to add the changes to the last commit.

---
+++ palettes.js
@@ -0,0 +1,56 @@
+export {getPalette};
+
+function getPalette() {
+    return dark_pink;
+}
+
+const main_dark = {
+    foreground: '#08080A',
+    background: '#0D0A0B',
+    highlight: '#F0E100',
+    accent: '#AA767C',
+    detail: '#50808E',
+    content: '#F0F7F4'
+};
+
+const dark_pink = {
+    foreground: '#070003',
+    background: '#0F0008',
+    highlight: '#F0E100',
+    accent: '#FFFAFD',
+    detail: '#FFFAFD',
+    content: '#FFFAFD'
+};
+
+
+const dark2 = {
+    background: '#1A0E13',
+    highlight: '#D4AF37',
+    accent: '#E4B7C0',
+    detail: '#F3E6D9',
+    content: '#F0E8E3'
+};
+
+const dark3 = {
+    background: '#1B0B15',
+    highlight: '#D7C4BC',
+    accent: '#C9A1B7',
+    detail: '#B3A29E',
+    content: '#EDEAE8'
+};
+
+const dark4 = {
+    background: '#191113',
+    highlight: '#C98F7B',
+    accent: '#E3C3C7',
+    detail: '#D1B5A7',
+    content: '#F4EDEA'
+};
+
+const dark_green = {
+    background: '#181C14',
+    highlight: '#697565',
+    accent: '#E3C3C7',
+    detail: '#ECDFCC',
+    content: '#ECDFCC'
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config}; */
+import {getPalette} from "./palettes.js";
+
+const palette = getPalette();
+
 module.exports = {
   content: [
     "./src/*.{js,html}",
@@ -10,50 +14,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'not-pearl': '#030316', // not rock
-        'less-black': '#23233c',
-        'lesser-black': '#424258',
-        'lesserer-black': '#626274',
-        'aiaia': '#a1a1ab',
-        'cococ': '#c0c0c7',
-        'eoeoe': '#e0e0e3',
-        'not-bell': '#9797c9',
-        'shadest-cata': '#3b1e05',
-        'shaderer-cata': '#2c1704',
-        'shader-cata': '#1d0f03',
-        'shade-cata': '#0f0801',
-        'tintest-cata': '#a98666',
-        'tinterer-cata': '#bb9e85',
-        'tinter-cata': '#ccb6a3',
-        'tint-cata': '#ddcfc2',
-        'clear-cata': '#ebecec',
-        'darkest-cata': '#01080f',
-        'darkerer-cata': '#03111d',
-        'darker-cata': '#04192c',
-        'dark-cata': '#05223b',
-        'not-cata': '#0a4376',
-        'less-cata': '#295b87',
-        'lesser-cata': '#477298',
-        'lesserer-cata': '#668aa9',
-        'mini-cata': '#85a1bb',
-        'minini-cata': '#a3b9cc',
-        'mininini-cata': '#c2d0dd',
-        'purple-cata': '#580a76',
-        'purpler-cata': '#3d0a76',
-        'blue-cata': '#220a76',
-        'bluer-cata': '#0a0d76',
-        'bluerer-cata': '#0a2876',
-        'teal-cata': '#0a4376',
-        'lesserer-pie': '#3d0a76',
-        'lesser-pie': '#350967',
-        'less-pie': '#2e0859',
-        'pie': '#26064a',
-        'dark-pie': '#1e053b',
-        'darker-pie': '#17042c',
-        'darkerer-pie': '#0f031d',
-
-
-
+        'background': palette.background,
+        'foreground': palette.foreground,
+        'border': palette.highlight,
+        'highlight': palette.highlight,
+        'text': palette.content,
+        'detail': palette.detail,
+        'accent': palette.accent
       },
       dropShadow: {
         'bg': '0 0 0 rgba(3, 3, 22, 1)',
